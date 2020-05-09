a405b563f2b8c8eac934666e21038baa06b62a66
s-janibekova <s.t.janibekova@gmail.com>
2020-05-09 18:23:02 +0600

    finished the first section

---
+++ my_profile/index.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+    typingText()
+    subtext() 
+});
+
+function typingText() {
+    const texts = ["I am Web developer" ,"I Have a lot of hobbies",]
+    const display = document.querySelector('.mainText');
+    let textIndex = 0;
+    let characterIndex = 0;
+    display.classList.add('typing');
+    setTimeout(() => {
+        
+        setInterval(() => {
+      
+            if (characterIndex === texts[textIndex].length) {
+              characterIndex = 0;
+              if (textIndex === texts.length - 1) {
+                textIndex = 0;
+              } else {
+                textIndex++;
+              }
+              display.innerHTML = '';
+              return;
+            }
+          
+            display.innerHTML += texts[textIndex][characterIndex];
+            characterIndex++;
+            
+          }, 150);
+
+    },4500)
+
+}
