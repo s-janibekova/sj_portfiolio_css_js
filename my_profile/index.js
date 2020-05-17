cafe21783f8301e8187e29aa6bfa16cf46483fb8
s-janibekova <s.t.janibekova@gmail.com>
2020-05-17 22:35:26 +0600

    refactoring used sass

--- my_profile/index.js
+++ my_profile/index.js
@@ -1,18 +1,18 @@
 $( document ).ready(function() {
     typingText()
-    subtext() 
+    subtext()
 });
 
 function typingText() {
-    const texts = ["I am Web developer" ,"I Have a lot of hobbies",]
+    const texts = ["I am Web developer" , "I Have a lot of hobbies",]
     const display = document.querySelector('.mainText');
     let textIndex = 0;
     let characterIndex = 0;
     display.classList.add('typing');
     setTimeout(() => {
-        
+
         setInterval(() => {
-      
+
             if (characterIndex === texts[textIndex].length) {
               characterIndex = 0;
               if (textIndex === texts.length - 1) {
@@ -23,12 +23,12 @@ function typingText() {
               display.innerHTML = '';
               return;
             }
-          
+
             display.innerHTML += texts[textIndex][characterIndex];
             characterIndex++;
-            
+
           }, 150);
 
-    },4500)
+    },2000)
 
 }
